75703bb6fe102e7df46cfdb5655337a7bc66ba09
AlvinElves <hong3842@gmail.com>
2022-11-08 22:39:37 +0000

    Outline of Home Page is done

--- GUI.py
+++ GUI.py
@@ -4,9 +4,9 @@ from tkinter import ttk
 LARGEFONT = ("Verdana", 35)
 
 
-class tkinterApp(tk.Tk):
+class GUI(tk.Tk):
 
-    # __init__ function for class tkinterApp
+    # __init__ function for class GUI
     def __init__(self, *args, **kwargs):
         # __init__ function for class Tk
         tk.Tk.__init__(self, *args, **kwargs)
@@ -14,7 +14,8 @@ class tkinterApp(tk.Tk):
         # creating a container
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
-        self.resizable(False, False)
+        #self.resizable(False, False)
+        self.title('Smart City Visualisation')
         window_Width = self.winfo_screenwidth()
         window_Height = self.winfo_screenheight()
         app_Width = 1100
@@ -31,54 +32,44 @@ class tkinterApp(tk.Tk):
         # initializing frames to an empty array
         self.frames = {}
 
-        # iterating through a tuple consisting
-        # of the different page layouts
-        for F in (StartPage, Page1, Page2):
+        # iterating through a tuple consisting of the different page layouts
+        for F in (HomePage, Page1, Page2):
             frame = F(container, self)
 
-            # initializing frame of that object from
-            # startpage, page1, page2 respectively with
-            # for loop
+            # initializing frame of that object from all the pages within the for loop
             self.frames[F] = frame
 
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(HomePage)
 
-    # to display the current frame passed as
-    # parameter
+    # to display the current frame passed as parameter
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 
-# first window frame startpage
-
-class StartPage(tk.Frame):
+# HomePage window frame
+class HomePage(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg='black')
 
-        # label of frame Layout 2
-        label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+        # Create left and right frames
+        left_frame = tk.Frame(self, width=225, height=694, bg='royalblue', highlightbackground="deepskyblue", highlightthickness=5)
+        left_frame.grid(row=0, column=0, padx=2, pady=3)
 
-        # putting the grid in its place by using
-        # grid
-        label.grid(row=0, column=4, padx=10, pady=10)
+        right_frame = tk.Frame(self, width=865, height=694, bg='deepskyblue', highlightbackground="royalblue", highlightthickness=5)
+        right_frame.grid(row=0, column=1, padx=2, pady=3)
 
-        button1 = ttk.Button(self, text="Page 1",
-                             command=lambda: controller.show_frame(Page1))
+        label = tk.Label(right_frame, text="Home Page", width=23, font=('Raleway', 35, 'bold'), bg='deepskyblue')
+        label.grid(row=0, column=0, padx=42, pady=15)
 
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
+        right_inside_frame = tk.Frame(right_frame, width=755, height=584, bg='lightskyblue')
+        right_inside_frame.grid(row=1, column=0, pady=5, padx=50)
 
-        ## button to show frame 2 with text layout2
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(Page2))
+        #label = tk.Label(right_inside_frame, text="HOME PAGE\n\n\n\n\n\n\n\n\n", width=23, font=LARGEFONT, bg='lightskyblue')
+        #label.grid(row=0, column=0, padx=42, pady=6)
 
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
 
 
 # second window frame page1
@@ -92,7 +83,7 @@ class Page1(tk.Frame):
         # button to show frame 2 with text
         # layout2
         button1 = ttk.Button(self, text="StartPage",
-                             command=lambda: controller.show_frame(StartPage))
+                             command=lambda: controller.show_frame(HomePage))
 
         # putting the button in its place
         # by using grid
@@ -127,15 +118,14 @@ class Page2(tk.Frame):
         # button to show frame 3 with text
         # layout3
         button2 = ttk.Button(self, text="Startpage",
-                             command=lambda: controller.show_frame(StartPage))
+                             command=lambda: controller.show_frame(HomePage))
 
         # putting the button in its place by
         # using grid
         button2.grid(row=2, column=1, padx=10, pady=10)
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app = tkinterApp()
-    app.mainloop()
+    software = GUI()
+    software.mainloop()
